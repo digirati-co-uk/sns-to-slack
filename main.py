97af0048e6537ed92e822be689db14662d19d7c7
Daniel Grant <daniel.grant@digirati.com>
2021-12-08 15:37:35 +0000

    Upgraded from Python 2 to Python 3

--- main.py
+++ main.py
@@ -4,17 +4,20 @@ import os
 
 
 def alert_slack(webhook_url, message):
-    _ = requests.post(webhook_url, json={
+    _ = requests.post(
+        webhook_url,
+        json={
             "text": message["headline"],
             "link_names": 1,
             "attachments": [
                 {
                     "text": message["detail"],
                     "color": message["color"],
-                    "title": message["title"]
+                    "title": message["title"],
                 }
-            ]
-        })
+            ],
+        },
+    )
 
 
 def lambda_handler(event, context):
@@ -22,13 +25,17 @@ def lambda_handler(event, context):
     webhook_url = os.getenv("SLACK_WEBHOOK_URL")
 
     message = {
-        "headline": event["Records"][0]["Sns"]["MessageAttributes"]["headline"]["Value"],
+        "headline": event["Records"][0]["Sns"]["MessageAttributes"]["headline"][
+            "Value"
+        ],
         "detail": event["Records"][0]["Sns"]["Message"],
         "color": event["Records"][0]["Sns"]["MessageAttributes"]["color"]["Value"],
-        "title": event["Records"][0]["Sns"]["MessageAttributes"]["title"]["Value"]
+        "title": event["Records"][0]["Sns"]["MessageAttributes"]["title"]["Value"],
     }
 
-    print("about to post message " + json.dumps(message))
+    print(("raw message: " + event["Records"][0]["Sns"]["Message"]))
+
+    print(("about to post message " + json.dumps(message)))
 
     alert_slack(webhook_url, message)
 
